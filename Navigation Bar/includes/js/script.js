06b2225b8ae1c506d7f10cdafdc7b11f5a073f74
saralkochar <saralkochar@gmail.com>
2015-01-12 19:10:39 +0800

    Navigation Bar

--- Navigation Bar/includes/js/script.js
+++ Navigation Bar/includes/js/script.js
@@ -1,10 +1,24 @@
 /*
 
-My Custom JS
-============
-
-Author:  Brad Hussey
-Updated: August 2013
-Notes:	 Hand coded for Udemy.com
 
 */
+
+$(function() {
+
+  $('#alertMe').click(function(e) {
+
+    e.preventDefault();
+
+    $('#successAlert').slideDown();
+
+  });
+
+  $('#alertMe2').click(function(e) {
+
+    e.preventDefault();
+
+    $('#warningAlert').slideDown();
+
+  });
+
+});
---
+++ Navigation Bar/readme.txt
@@ -0,0 +1,8 @@
+It’s the first attempt of creating a Navigation Bar using Bootstrap3, HTML5 and CSS3.
+
+Features: 
+- Responsive.
+- Fevicon on header included.  
+- All files included. 
+
+You can see how it works at below link: 
